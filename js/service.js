15216a108f29bdf96b96cfae6fc5cee3c0e1f9b7
eli-bernardi <elielbernardi0012@gmail.com>
2025-12-10 07:49:15 -0300

    Delete mobile menu toggle event listener
    Removed mobile menu toggle functionality.

--- js/service.js
+++ js/service.js
@@ -25,12 +25,3 @@ document.addEventListener('DOMContentLoaded', function () {
             updateCarousel();
         }
     });
-
-    // Menu mobile
-    const menuToggle = document.getElementById('menu-toggle');
-    const menu = document.getElementById('menu');
-
-    menuToggle.addEventListener('click', function () {
-        menu.classList.toggle('hidden');
-    });
-});
